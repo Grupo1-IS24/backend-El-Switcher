ffcc38c32f1ce3c5bcb61066e6684c1e96e37cfb
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-11-01 18:25:03 -0300

    SCRUM-257 Almaceno el timer

--- app/services/timer.py
+++ app/services/timer.py
@@ -5,17 +5,20 @@ from app.services import game
 
 TURN_TIME_LIMIT = 120
 
-# Global dictionary to keep track of timer tasks
+# Global dictionary to keep track of timer tasks and remaining time
 timer_tasks = {}
+time_left_dict = {}
 
 
-async def handle_timer(game_id, player_id, db):
-
+async def handle_timer(game_id, player_id, db, reset=False):
     # Cancel the previous timer task if it exists
     if game_id in timer_tasks:
-        # Cancel the previous timer task
         timer_tasks[game_id].cancel()
         del timer_tasks[game_id]
+
+    # Reset the timer if specified
+    if reset or game_id not in time_left_dict:
+        time_left_dict[game_id] = TURN_TIME_LIMIT
 
     # Start the timer for the new turn
     timer_tasks[game_id] = asyncio.create_task(
@@ -24,15 +27,16 @@ async def handle_timer(game_id, player_id, db):
 
 
 async def emit_timer(game_id, player_id, db):
-
     broadcast = Broadcast()
-    time_left = TURN_TIME_LIMIT
+    time_left = time_left_dict[game_id]
 
     while time_left > 0:
+        time_left_dict[game_id] = time_left
         await broadcast.broadcast(
             sio.sio_game, game_id, "timer", {"time": time_left}
         )
         await asyncio.sleep(1)
         time_left -= 1
 
+    del time_left_dict[game_id]
     await game.end_turn(game_id, player_id, db)
